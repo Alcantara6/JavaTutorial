f28a4a49510e9edd4bcb62a2b8f36074dd6c2d2e
yanjing <yanjing@deepdraw.cn>
2021-11-08 18:05:07 +0800

    前端history模式，后端配置自定义404页面

---
+++ springboot-vue/app/src/main/java/com/yanjing/error/ErrorConfig.java
@@ -0,0 +1,23 @@
+package com.yanjing.error;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yanjing
+ * @date 2021/11/8
+ * 前端history模式，跳转自定义404页面
+ */
+@Component
+public class ErrorConfig implements ErrorPageRegistrar {
+
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+
+        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+        registry.addErrorPages(error404Page);
+    }
+}
--- springboot-vue/web/vue.config.js
+++ springboot-vue/web/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	publicPath: '.',
 	configureWebpack: (config) => {
 		config.devServer = {
 			disableHostCheck: true,
