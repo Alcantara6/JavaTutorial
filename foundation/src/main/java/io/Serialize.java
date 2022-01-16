5d03aa3e4729ae8ff37a0da16b79ece612d10ff6
yanjing <yanjing@deepdraw.cn>
2022-01-16 12:44:22 +0800

    serialize demo

---
+++ foundation/src/main/java/io/Serialize.java
@@ -0,0 +1,52 @@
+package io;
+
+import java.io.*;
+
+/**
+ * @author yanjing
+ * @date 2022/1/6
+ */
+public class Serialize {
+
+    public static void main(String[] args) {
+
+        writeUser();
+        readUser();
+    }
+
+    private static void writeUser() {
+
+        User user = new User();
+        user.setId(1);
+        user.setName("张三");
+        user.setAge(12);
+
+        try {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(new File("./stream.txt")));
+            stream.writeObject(user);
+            System.out.println("序列化成功");
+            stream.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private static void readUser() {
+
+        try {
+
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream("./stream.txt"));
+            try {
+               User user = (User) stream.readObject();
+               System.out.println("反序列化成功");
+               System.out.println(user);
+            } catch (ClassNotFoundException e) {
+
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ foundation/src/main/java/io/User.java
@@ -0,0 +1,42 @@
+package io;
+
+import java.io.Serializable;
+
+/**
+ * @author yanjing
+ * @date 2022/1/6
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -3340693206126193734L;
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
