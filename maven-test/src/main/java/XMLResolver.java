3587004305b1976b0be48e3d18865326f7b3c11f
yanjing <yanjing@deepdraw.cn>
2022-09-12 19:29:43 +0800

    maven-test 不同包路径相同的加载顺序

---
+++ maven-test/src/main/java/XMLResolver.java
@@ -0,0 +1,13 @@
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+
+/**
+ * @author yanjing
+ * @date 2022/8/16
+ */
+public class XMLResolver {
+
+    public static void main(String[] args) throws DocumentException {
+        DocumentHelper.parseText("abc");
+    }
+}
